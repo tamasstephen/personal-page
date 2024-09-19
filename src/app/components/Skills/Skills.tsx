d6f151947d161226d13053d33395bc2791cc06cb
tamasstephen <kicsitnagyon@gmail.com>
2024-09-19 11:10:22 +0200

    feat: add skills

--- src/app/components/Skills/Skills.tsx
+++ src/app/components/Skills/Skills.tsx
@@ -1,7 +1,5 @@
 import { Wrapper } from "../Wrapper";
-import Image from "next/image";
-import TerminalIcon from "@/assets/terminal.svg";
-import MonitorIcon from "@/assets/monitor.svg";
+import { ToolBox } from "./components";
 
 export const Skills = () => {
   return (
@@ -11,46 +9,8 @@ export const Skills = () => {
           My Development Toolkit.
         </h2>
         <div className="mt-10 lg:mt-16 grid gap-10 md:grid-cols-2">
-          <div className="rounded-md overflow-hidden md:h-full">
-            <div className="border-2 md:h-full [border-image:linear-gradient(to_top,#C0B8A0,#BCC3EB)_30]  p-6">
-              <p className="flex text-xl font-bold">
-                <Image src={TerminalIcon} alt="terminal icon" />
-                <span className="ml-2">Languages I speak</span>
-              </p>
-              <ul className="list-disc pl-6 grid gap-2 mt-6">
-                <li>HTML</li>
-                <li>CSS</li>
-                <li>JavaScript</li>
-                <li>TypeScript</li>
-                <li>Sass</li>
-                <li>React</li>
-                <li>Hungarian</li>
-                <li>English</li>
-                <li>German</li>
-              </ul>
-            </div>
-          </div>
-          <div className="rounded-md overflow-hidden">
-            <div className="border-2 [border-image:linear-gradient(to_top,#BCC3EB,#C0B8A0)_30]  p-6">
-              <p className="flex text-xl font-bold">
-                <Image src={MonitorIcon} alt="monitor icon" />
-                <span className="ml-2">Tools I use</span>
-              </p>
-              <ul className="list-disc pl-6 grid gap-2 mt-6">
-                <li>Git</li>
-                <li>Tailwind</li>
-                <li>NextJs</li>
-                <li>Github</li>
-                <li>Docker</li>
-                <li>Vercel</li>
-                <li>Express Js</li>
-                <li>Jest</li>
-                <li>React Testing Library</li>
-                <li>Figma</li>
-                <li>Supabase</li>
-              </ul>
-            </div>
-          </div>
+          <ToolBox isFirst />
+          <ToolBox />
         </div>
       </section>
     </Wrapper>
