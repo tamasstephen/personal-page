ab1da62035fc3aa125542a0e3f88a811c9296a4d
tamasstephen <kicsitnagyon@gmail.com>
2024-09-23 22:46:22 +0200

    chore: update robots.txt and sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://personal-page-blue-ten.vercel.app/sitemap.xml",
+    sitemap: "https://tamasstephen.com/sitemap.xml",
   };
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -4,13 +4,13 @@ import { projects } from "./data";
 export default function sitemap(): MetadataRoute.Sitemap {
   const slugs = Object.keys(projects);
   const pages = slugs.map((slug) => ({
-    url: "https://personal-page-blue-ten.vercel.app/" + slug,
+    url: "https://tamasstephen.com" + slug,
     lastModified: new Date(),
     priority: 2,
   }));
   return [
     {
-      url: "https://personal-page-blue-ten.vercel.app",
+      url: "https://tamasstephen.com",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
